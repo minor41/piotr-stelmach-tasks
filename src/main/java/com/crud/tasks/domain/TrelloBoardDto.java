46491cbe601e77129fc9b171f9443a41299fa326
piotrstelmach <minor41@gmail.com>
2018-04-06 14:36:05 +0200

    18.2

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+
+    public TrelloBoardDto() {
+    }
+}
