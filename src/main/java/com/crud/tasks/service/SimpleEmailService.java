ad5662050d52ab52e2796c658e06e03d20fed2bd
piotrstelmach <minor41@gmail.com>
2018-04-14 01:17:14 +0200

    19.2

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -1,4 +1,41 @@
 package com.crud.tasks.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleEmailService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void send(final String receiverEmail, final String subject, final String message) {
+
+        LOGGER.info("Starting email preparation...");
+
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(receiverEmail, subject, message);
+            javaMailSender.send(mailMessage);
+
+            LOGGER.info("Message has been sent.");
+        } catch (MailException e) {
+            LOGGER.error("Failed to process email sending:", e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final String receiverEmail, final String subject,
+                                                final String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(receiverEmail);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        return mailMessage;
+    }
 }
