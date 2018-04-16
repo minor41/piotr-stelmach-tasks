d7644d5aae7da827000cdd6b3542d37177c0723e
piotrstelmach <minor41@gmail.com>
2018-04-16 20:09:28 +0200

    19,3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -13,7 +13,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/v1/task/")
+@RequestMapping("/v1/task")
 public class TaskController {
 
     @Autowired
